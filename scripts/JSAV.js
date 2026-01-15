91909ca97d3af981d4400128983cb1602f668401
fsnibs <fsatnibs@163.com>
2026-01-15 15:56:02 +0800

    modify Dismiss as Download

--- scripts/JSAV.js
+++ scripts/JSAV.js
@@ -2615,12 +2615,7 @@ function ACRM_dialog(title, msg, width, pre)
         resizable: false,
         width: width,
         modal: true,
-        buttons: {
-            "Dismiss": function() {
-                $( this ).dialog( "close" );
-                $( this ).remove();
-            }
-        }
+        buttons: { "Download": function() { var urlParams = new URLSearchParams(window.location.search); var id = urlParams.get("id"); var filename = "alignment.fasta"; if (id) { filename = id + ".amino_acid_alignment.fasta"; } var blob = new Blob([msg], { type: "text/plain" }); var link = document.createElement("a"); link.href = window.URL.createObjectURL(blob); link.download = filename; document.body.appendChild(link); link.click(); document.body.removeChild(link); $( this ).dialog( "close" ); $( this ).remove(); } }
     });
 };
 
@@ -3427,3 +3422,5 @@ return(html);
 }
 
 // --------------------- END OF FILE ------------------------------------
+
+
